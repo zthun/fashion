8b6757352586209c38e93fc0c9fcdd43fd0d4e42
Anthony Bonta <zthun.coding@gmail.com>
2025-06-21 11:08:35 -0500

    chore: cleanup

--- packages/fashion-boutique/src/dialog/use-dialog.tsx
+++ packages/fashion-boutique/src/dialog/use-dialog.tsx
@@ -57,7 +57,6 @@ export function useDialog(
 
   // This next bit is nearly impossible to test without modification
   // to circus to click on an offset of the body.  Can be done, but needs to come later.
-  /* istanbul ignore next --@preserve */
   const closeOnBackdropClick = (e: MouseEvent) => {
     const r = e.currentTarget.getBoundingClientRect();
 
