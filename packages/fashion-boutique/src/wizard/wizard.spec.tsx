956b31a42073ff97ccec7918a2251fb2f08ddc0a
Anthony Bonta <zthun.coding@gmail.com>
2025-06-21 12:49:00 -0500

    test: empty children

--- packages/fashion-boutique/src/wizard/wizard.spec.tsx
+++ packages/fashion-boutique/src/wizard/wizard.spec.tsx
@@ -1,8 +1,10 @@
 import type { IZCircusDriver, IZCircusSetup } from "@zthun/cirque";
 import { ZCircusBy } from "@zthun/cirque";
 import { ZCircusSetupRenderer } from "@zthun/cirque-du-react";
+import { firstDefined } from "@zthun/helpful-fn";
 import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import { ZWizardComponentModel } from "./wizard.cm.mjs";
+import type { IZWizard } from "./wizard.js";
 import { ZWizard } from "./wizard.js";
 
 describe("ZWizard", () => {
@@ -11,21 +13,24 @@ describe("ZWizard", () => {
   let _renderer: IZCircusSetup<IZCircusDriver>;
   let _driver: IZCircusDriver;
 
-  const createTestTarget = async () => {
-    const element = (
-      <ZWizard>
+  const createTestTarget = async ({ children, ...props }: IZWizard = {}) => {
+    const _children = firstDefined(
+      [
         <div data-name="Page 1" data-next-disabled={page1Disabled}>
           Page 1
-        </div>
-        <div data-name="Page 2">Page 2</div>
+        </div>,
+        <div data-name="Page 2">Page 2</div>,
         <div data-name="Page 3" data-next-disabled={lastPageDisabled}>
           Page 3
-        </div>
-      </ZWizard>
+        </div>,
+      ],
+      children,
     );
+    const element = <ZWizard {...props}>{_children}</ZWizard>;
 
     _renderer = new ZCircusSetupRenderer(element);
     _driver = await _renderer.setup();
+
     return ZCircusBy.first(_driver, ZWizardComponentModel);
   };
 
@@ -46,21 +51,41 @@ describe("ZWizard", () => {
         const target = await createTestTarget();
         const current = await target.page();
         const next = await target.next();
+
         // Act.
         await next?.click();
         const actual = await target.page();
+
         // Assert.
         expect(actual).toEqual(current + 1);
       });
 
       it("should be hidden on the last page", async () => {
         // Arrange.
-        const target = await createTestTarget();
+        const target = await createTestTarget({
+          CardProps: {},
+          PrevButtonProps: {},
+          NextButtonProps: {},
+          FinishButtonProps: {},
+        });
         const next = await target.next();
+
         // Act.
         await next?.click();
         await next?.click();
         const actual = await target.next();
+
+        // Assert.
+        expect(actual).toBeNull();
+      });
+
+      it("should be hidden if the children list is empty", async () => {
+        // Arrange.
+        const target = await createTestTarget({ children: [] });
+
+        // Act.
+        const actual = await target.next();
+
         // Assert.
         expect(actual).toBeNull();
       });
@@ -102,6 +127,18 @@ describe("ZWizard", () => {
         // Assert.
         expect(actual).toBeTruthy();
       });
+
+      it("should be disabled if the children list is empty", async () => {
+        // Arrange.
+        const target = await createTestTarget({ children: [] });
+        const previous = await target.previous();
+
+        // Act.
+        const actual = await previous.disabled();
+
+        // Assert.
+        expect(actual).toBeTruthy();
+      });
     });
 
     describe("Finish", () => {
@@ -109,10 +146,12 @@ describe("ZWizard", () => {
         // Arrange.
         const target = await createTestTarget();
         const next = await target.next();
+
         // Act
         await next?.click();
         await next?.click();
         const actual = await target.finish();
+
         // Assert.
         expect(actual).toBeTruthy();
       });
@@ -120,8 +159,21 @@ describe("ZWizard", () => {
       it("should be hidden before the last page.", async () => {
         // Arrange.
         const target = await createTestTarget();
+
         // Act.
         const actual = await target.finish();
+
+        // Assert.
+        expect(actual).toBeNull();
+      });
+
+      it("should be hidden if the child list is empty", async () => {
+        // Arrange.
+        const target = await createTestTarget({ children: [] });
+
+        // Act.
+        const actual = await target.finish();
+
         // Assert.
         expect(actual).toBeNull();
       });
@@ -133,9 +185,11 @@ describe("ZWizard", () => {
         const next = await target.next();
         await next?.click();
         await next?.click();
+
         // Act.
         const finish = await target.finish();
         const actual = await finish?.disabled();
+
         // Assert.
         expect(actual).toBeTruthy();
       });
