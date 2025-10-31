14e6518ae48228bfed8c1e689ceffa7033b6c845
Anthony Bonta <zthun.coding@gmail.com>
2025-10-30 22:56:34 -0500

    refactor: image page should keep just the one image
    It's just centered now

--- packages/fashion-web/src/boutique/image/image-page.tsx
+++ packages/fashion-web/src/boutique/image/image-page.tsx
@@ -11,20 +11,14 @@ import {
 } from "@zthun/fashion-boutique";
 import type { ZSize } from "@zthun/fashion-tailor";
 import { ZSizeFixed, ZSizeVaried } from "@zthun/fashion-tailor";
-import { ZDataUrlBuilder, ZUrlBuilder } from "@zthun/webigail-url";
+import { ZOrientation } from "@zthun/helpful-fn";
+import { ZUrlBuilder } from "@zthun/webigail-url";
 import type { Property } from "csstype";
 import { useMemo, useState } from "react";
 import { ZFashionRouteImage } from "../../routes.mjs";
 import { ZChoiceDropDownSize } from "../common/choice-drop-down-size.js";
 
-const Svg =
-  '<svg focusable="false" viewBox="0 0 24 24" fill="white"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path></svg>';
 const Png = new ZUrlBuilder().gravatar().build();
-const Data = new ZDataUrlBuilder()
-  .buffer(Svg)
-  .encode("base64")
-  .mimeType("image/svg+xml")
-  .build();
 
 export function ZImagePage() {
   const [width, setWidth] = useState<ZSize>();
@@ -62,14 +56,13 @@ export function ZImagePage() {
           this way, then it is directly added to the browser.
         </ZParagraph>
 
-        <ZGrid columns={{ xl: "1fr 1fr", xs: "1fr" }} gap={ZSizeFixed.Medium}>
-          <ZStack justify={{ content: "center" }} align={{ items: "center" }}>
-            <ZImage src={Png} width={width} height={height} fit={fit} />
-          </ZStack>
-          <ZStack justify={{ content: "center" }} align={{ items: "center" }}>
-            <ZImage src={Data} width={width} height={height} fit={fit} />
-          </ZStack>
-        </ZGrid>
+        <ZStack
+          orientation={ZOrientation.Horizontal}
+          justify={{ content: "center" }}
+          width={ZSizeVaried.Full}
+        >
+          <ZImage src={Png} width={width} height={height} fit={fit} />
+        </ZStack>
       </ZBox>
 
       <ZH3>Options</ZH3>
