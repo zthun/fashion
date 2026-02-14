272d839e7a569d7e9a877b94764bc7f075349003
Anthony Bonta <zthun.coding@gmail.com>
2026-02-14 16:13:21 -0600

    refactor: separate theme page into categories

--- packages/fashion-web/src/theme/theme-page.tsx
+++ packages/fashion-web/src/theme/theme-page.tsx
@@ -1,10 +1,11 @@
 import {
-  ZBox,
   ZCard,
   ZGrid,
   ZH3,
+  ZH4,
   ZIconFontAwesome,
   ZParagraph,
+  ZStack,
   useFashionTheme,
 } from "@zthun/fashion-boutique";
 import { ZSizeFixed } from "@zthun/fashion-tailor";
@@ -45,33 +46,55 @@ export function ZThemePage() {
         ),
       }}
     >
-      <ZBox margin={{ bottom: ZSizeFixed.Large }}>
-        <ZH3>Description</ZH3>
-
-        <ZParagraph>
+      <ZStack gap={ZSizeFixed.ExtraLarge}>
+        <ZParagraph compact>
           A fashion theme describes the different makeup of page colors that are
           used across common places in a website.
         </ZParagraph>
 
-        <ZBox margin={{ bottom: ZSizeFixed.Medium }}>
-          <ZGrid
-            columns={{ xl: "1fr 1fr 1fr", lg: "1fr 1fr", md: "1fr" }}
-            gap={ZSizeFixed.Medium}
-          >
-            <ZFashionColors fashion={primary} />
-            <ZFashionColors fashion={secondary} />
-            <ZFashionColors fashion={success} />
-            <ZFashionColors fashion={warning} />
-            <ZFashionColors fashion={error} />
-            <ZFashionColors fashion={info} />
-            <ZFashionColors fashion={light} />
-            <ZFashionColors fashion={dark} />
-            <ZFashionColors fashion={body} />
-            <ZFashionColors fashion={surface} />
-            <ZFashionColors fashion={component} />
-          </ZGrid>
-        </ZBox>
-      </ZBox>
+        <ZH3 compact>Priority</ZH3>
+
+        <ZGrid
+          columns={{ xl: "1fr 1fr 1fr", lg: "1fr 1fr", md: "1fr" }}
+          gap={ZSizeFixed.Medium}
+        >
+          <ZFashionColors fashion={primary} />
+          <ZFashionColors fashion={secondary} />
+        </ZGrid>
+
+        <ZH4 compact>Severity</ZH4>
+
+        <ZGrid
+          columns={{ xl: "1fr 1fr 1fr", lg: "1fr 1fr", md: "1fr" }}
+          gap={ZSizeFixed.Medium}
+        >
+          <ZFashionColors fashion={success} />
+          <ZFashionColors fashion={warning} />
+          <ZFashionColors fashion={error} />
+          <ZFashionColors fashion={info} />
+        </ZGrid>
+
+        <ZH4 compact>Area</ZH4>
+
+        <ZGrid
+          columns={{ xl: "1fr 1fr 1fr", lg: "1fr 1fr", md: "1fr" }}
+          gap={ZSizeFixed.Medium}
+        >
+          <ZFashionColors fashion={body} />
+          <ZFashionColors fashion={surface} />
+          <ZFashionColors fashion={component} />
+        </ZGrid>
+
+        <ZH4>Contrast</ZH4>
+
+        <ZGrid
+          columns={{ xl: "1fr 1fr 1fr", lg: "1fr 1fr", md: "1fr" }}
+          gap={ZSizeFixed.Medium}
+        >
+          <ZFashionColors fashion={light} />
+          <ZFashionColors fashion={dark} />
+        </ZGrid>
+      </ZStack>
     </ZCard>
   );
 }
