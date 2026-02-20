8af254eda20f691a4ccacf06b48109d1b30e8cc8
Anthony Bonta <zthun.coding@gmail.com>
2026-02-19 22:29:07 -0600

    test: dark theme

---
+++ packages/fashion-theme-dark/src/theme/create-theme-dark.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { createThemeDark } from "./create-theme-dark.mjs";
+
+describe("Dark Theme", () => {
+  it("should create the theme", () => {
+    expect(createThemeDark()).toBeTruthy();
+  });
+});
