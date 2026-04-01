03da8bd65d4d553ea9d600790815e9276288a662
Anthony Bonta <zthun.coding@gmail.com>
2026-04-01 07:16:41 -0500

    build: update janitor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,6 @@ import { environments, react, recommended } from "@zthun/janitor-eslint-config";
 export default [
   ...recommended,
   ...react,
-  {
-    rules: {
-      "@typescript-eslint/unbound-method": "off",
-    },
-  },
   ...environments.node,
   ...environments.browser,
 ];
